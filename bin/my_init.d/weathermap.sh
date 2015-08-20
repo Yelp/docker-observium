20d38d7369c9a7e0cc151875573ca140effa01dc
Codey Oxley <codey@yelp.com>
2015-08-20 15:25:54 -0700

    Fixing cronjob for weather map

--- bin/my_init.d/weathermap.sh
+++ bin/my_init.d/weathermap.sh
@@ -6,7 +6,7 @@
 
 HTMLDIR='/opt/observium/html'
 WEATHERMAP="$HTMLDIR/weathermap"
-C='*/5 * * * * /opt/observium/html/weathermap/map-poller.php >> /dev/null 2>&1'
+C='*/5 *     * * *   root    /opt/observium/html/weathermap/map-poller.php >> /dev/null 2>&1'
 
 # Check env for cue to enable
 if [ "$USE_WEATHERMAP" = true ]; then
